5f5de94b48e7aab4af0537c4751f63a1b8e4fd1f
Phil Boyd <phil.boyd@live.com>
2018-12-31 12:02:05 -0500

    added breezejs module updated editorconfig

---
+++ ClientApp/src/app/breeze-js/breeze-js.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BreezeJSRoutingModule } from './breeze-js-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BreezeJSRoutingModule
+  ]
+})
+export class BreezeJSModule { }
