d2d62a6ee6aaae3ce6a77273a6a9794a08d8863c
Phil Boyd <phil.boyd@live.com>
2019-02-02 11:57:18 -0500

    87 complete

--- ClientApp/src/app/ultimate-angular/pro/state-management-ngrx/songs/songs.service.ts
+++ ClientApp/src/app/ultimate-angular/pro/state-management-ngrx/songs/songs.service.ts
@@ -5,6 +5,8 @@ import { of } from 'rxjs';
 import json from 'src/assets/json/ultimate-angular.json';
 import { tap, map } from 'rxjs/operators';
 import { Song } from './song';
+import _findIndex from 'lodash/findIndex';
+import _sortBy from 'lodash/sortBy';
 
 @Injectable({
     providedIn: 'root'
@@ -17,7 +19,11 @@ export class SongsService {
         // tap(results => console.log('results', results)),
         tap(results => this._store.set('playlist', results))
     );
-    toggle(song: Song): any {
-        console.log(song);
+    toggle(event: any): any {
+        const song = event.song;
+        const songs = this._store.value.playlist;
+        const index = _findIndex(songs, (s: Song) => s.id === song.id);
+        songs.splice(index, 1, song);
+        this._store.set('playlist', _sortBy(songs, (s: Song) => s.id));
     }
 }
