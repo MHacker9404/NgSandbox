e13fa933d39141a4020de52e8fd90592ac241aba
Phil Boyd <phil.boyd@live.com>
2019-03-15 11:25:49 -0400

    adding material-icons styling

--- ClientApp/src/app/angular-university/au-rxjs/components/browser-events/browser-events.component.ts
+++ ClientApp/src/app/angular-university/au-rxjs/components/browser-events/browser-events.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ngs-browser-events',
-  template: `
-    <p>
-      browser-events works!
-    </p>
-  `,
-  styles: []
+    selector: 'ngs-browser-events',
+    template: `
+        <div class="course-container">
+            <h2>Browser Events</h2>
+            <div id="_hover" class="hover-me"></div>
+        </div>
+    `,
+    styles: [],
 })
 export class BrowserEventsComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
