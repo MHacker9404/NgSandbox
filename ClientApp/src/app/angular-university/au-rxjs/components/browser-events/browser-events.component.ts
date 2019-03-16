9a6f6bdb3442d7331d3dad59db543e0a133aeabe
Phil Boyd <phil.boyd@live.com>
2019-03-16 09:15:08 -0400

    lesson 11 complete

--- ClientApp/src/app/angular-university/au-rxjs/components/browser-events/browser-events.component.ts
+++ ClientApp/src/app/angular-university/au-rxjs/components/browser-events/browser-events.component.ts
@@ -6,12 +6,28 @@ import { Component, OnInit } from '@angular/core';
         <div class="course-container">
             <h2>Browser Events</h2>
             <div id="_hover" class="hover-me"></div>
+            <button class="btn btn-primary btn-sm" (click)="unsubscribe()">Unsubscribe</button>
         </div>
     `,
-    styles: [],
+    styleUrls: ['../../au-rxjs.component.scss'],
 })
 export class BrowserEventsComponent implements OnInit {
+    _hoverSection: HTMLElement;
+
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this._hoverSection = document.getElementById('_hover');
+
+        this._hoverSection.addEventListener('mousemove', onMouseMove);
+    }
+
+    unsubscribe() {
+        console.log('Called unsubscribe()');
+        this._hoverSection.removeEventListener('mousemove', onMouseMove);
+    }
+}
+
+function onMouseMove(event: MouseEvent) {
+    console.log(event);
 }
