5f135f9960c9ee21121e6d1c31db2e8a5a2ffb9b
Phil Boyd <phil.boyd@live.com>
2019-07-18 10:45:03 -0400

    lesson 48 complete updated packages

--- ClientApp/src/app/angular-university/rxjs-reactive-programming/section-6/course-detail/course-detail.component.ts
+++ ClientApp/src/app/angular-university/rxjs-reactive-programming/section-6/course-detail/course-detail.component.ts
@@ -1,97 +1,94 @@
-import {Component, NgModule, OnInit, OnDestroy} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {SharedModule} from 'src/app/shared/shared.module';
-import {ActivatedRoute} from '@angular/router';
-import {DatastoreService} from '../datastore.service';
-import {ICourse} from '../../shared/model/icourse';
-import {ILesson} from '../../shared/model/ilesson';
-import {Observable, Subject} from 'rxjs';
-import {takeUntil, tap} from 'rxjs/operators';
+import { Component, NgModule, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ActivatedRoute } from '@angular/router';
+import { DatastoreService } from '../datastore.service';
+import { ICourse } from '../../shared/model/icourse';
+import { ILesson } from '../../shared/model/ilesson';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 import _find from 'lodash/find';
 import _cloneDeep from 'lodash/cloneDeep';
-import {tag} from 'rxjs-spy/operators/tag';
-import {CourseDetailHeaderModule} from '../course-detail-header/course-detail-header.component';
-import {NewsletterService} from '../newsletter.service';
+import { tag } from 'rxjs-spy/operators/tag';
+import { CourseDetailHeaderModule } from '../course-detail-header/course-detail-header.component';
+import { NewsletterService } from '../newsletter.service';
 
 @Component({
-  selector: 'ngs-course-detail',
-  template: `
-    <ngs-course-detail-header
-      [course]="course$ | async"
-      [lessons]="lessons$ | async"
-      firstName="John"
-      (subscribe)="onSubscribe($event)"
-    ></ngs-course-detail-header>
+    selector: 'ngs-course-detail',
+    template: `
+        <ngs-course-detail-header
+            [course]="course$ | async"
+            [lessons]="lessons$ | async"
+            firstName="John"
+            (subscribe)="onSubscribe($event)"
+        ></ngs-course-detail-header>
 
-    <table
-      class="table lessons-list card card-strong"
-      *ngIf="lessons$ | async as lessons; else lessonsLoading"
-    >
-      <tbody>
-        <tr *ngFor="let lesson of lessons">
-          <td class="lesson-title">{{ lesson.description }}</td>
-          <td class="duration">
-            <i class="material-icons">access_time</i>
-            <span>{{ lesson.duration }}</span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+        <table class="table lessons-list card card-strong" *ngIf="lessons$ | async as lessons; else lessonsLoading">
+            <tbody>
+                <tr *ngFor="let lesson of lessons">
+                    <td class="lesson-title">{{ lesson.description }}</td>
+                    <td class="duration">
+                        <i class="material-icons">access_time</i>
+                        <span>{{ lesson.duration }}</span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
 
-    <ng-template #lessonsLoading>
-      <div>Loading ...</div>
-    </ng-template>
-  `,
-  styleUrls: ['./course-detail.component.scss'],
-  })
+        <ng-template #lessonsLoading>
+            <div>Loading ...</div>
+        </ng-template>
+    `,
+    styleUrls: ['./course-detail.component.scss'],
+})
 export class CourseDetailComponent implements OnInit, OnDestroy {
-  course$: Observable<ICourse>;
-  lessons$: Observable<ILesson[]>;
-  private _unsubscribe$ = new Subject<void>();
+    course$: Observable<ICourse>;
+    lessons$: Observable<ILesson[]>;
+    private _unsubscribe$ = new Subject<void>();
 
-  //   course: ICourse;
-  //   lessons: ILesson[];
-  constructor(
-    private _route: ActivatedRoute,
-    private _dataStore: DatastoreService,
-    private _newsLetter: NewsletterService
-  ) {
-    let courseId: string;
-    _route.params
-        .pipe(
+    //   course: ICourse;
+    //   lessons: ILesson[];
+    constructor(
+        private _route: ActivatedRoute,
+        private _dataStore: DatastoreService,
+        private _newsLetter: NewsletterService
+    ) {
+        let courseId: string;
+        _route.params
+            .pipe(
+                takeUntil(this._unsubscribe$),
+                tap(params => (courseId = params['id'])),
+                tag('course-detail: route-params')
+            )
+            .subscribe();
+        this.course$ = this._dataStore.findCourseByUrl(courseId).pipe(
             takeUntil(this._unsubscribe$),
-            tap((params) => (courseId = params['id'])),
-            tag('course-detail: route-params')
-        )
-        .subscribe();
-    this.course$ = this._dataStore.findCourseByUrl(courseId).pipe(
-        takeUntil(this._unsubscribe$),
-        tag('course-detail: course')
-    );
-    this.lessons$ = this._dataStore.findLessonsForCourse(courseId).pipe(
-        takeUntil(this._unsubscribe$),
-        tag('course-detail: lessons')
-    );
-  }
+            tag('course-detail: course')
+        );
+        this.lessons$ = this._dataStore.findLessonsForCourse(courseId).pipe(
+            takeUntil(this._unsubscribe$),
+            tag('course-detail: lessons')
+        );
+    }
 
-  ngOnInit() {}
+    ngOnInit() {}
 
-  onSubscribe(email: string) {
-    this._newsLetter.subscribeToNewsletter(email).subscribe((data) => {
-      console.log(data);
-      alert(`Subscription successful ...`);
-    }, console.error);
-  }
+    onSubscribe(email: string) {
+        this._newsLetter.subscribeToNewsletter(email).subscribe(response => {
+            console.log(response);
+            alert(`Subscription successful ...${response.email}`);
+        }, console.error);
+    }
 
-  ngOnDestroy(): void {
-    this._unsubscribe$.next();
-    this._unsubscribe$.complete();
-  }
+    ngOnDestroy(): void {
+        this._unsubscribe$.next();
+        this._unsubscribe$.complete();
+    }
 }
 
 @NgModule({
-  declarations: [CourseDetailComponent],
-  imports: [CommonModule, SharedModule, CourseDetailHeaderModule],
-  exports: [CourseDetailComponent],
-  })
+    declarations: [CourseDetailComponent],
+    imports: [CommonModule, SharedModule, CourseDetailHeaderModule],
+    exports: [CourseDetailComponent],
+})
 export class CourseDetailModule {}
