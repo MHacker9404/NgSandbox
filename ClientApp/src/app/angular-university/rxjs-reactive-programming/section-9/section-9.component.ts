ed00cf5760748dd8d1e2e23c77b80905df7f2f37
Phil Boyd <phil.boyd@live.com>
2019-08-02 18:23:22 -0400

    Section 9 complete - Master Detail

--- ClientApp/src/app/angular-university/rxjs-reactive-programming/section-9/section-9.component.ts
+++ ClientApp/src/app/angular-university/rxjs-reactive-programming/section-9/section-9.component.ts
@@ -3,6 +3,19 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { TopMenuModule } from './top-menu/top-menu.component';
+import { HomeModule, HomeComponent } from './home/home.component';
+import { AllLessonsModule, AllLessonsComponent } from './all-lessons/all-lessons.component';
+import { CourseDetailModule } from './course-detail/course-detail.component';
+import { CoursesListModule } from './courses-list/courses-list.component';
+import { LessonsListModule } from '../with-rxjs/lessons-list/lessons-list.component';
+import { CourseDetailHeaderModule } from './course-detail-header/course-detail-header.component';
+import { NewsletterModule } from './newsletter/newsletter.component';
+import { LoginModule } from './login/login.component';
+import { CourseModule } from './course/course.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { LoginComponent } from './login/login.component';
+import { DatastoreService } from './datastore.service';
+import { LessonDetailModule } from './lesson-detail/lesson-detail.component';
 
 @Component({
     selector: 'ngs-section-9',
@@ -26,26 +39,26 @@ const routes: Routes = [
         path: '',
         component: Section9Component,
         children: [
-            // {
-            //     path: 'course/:id',
-            //     component: CourseDetailComponent,
-            // },
-            // {
-            //     path: 'login',
-            //     component: LoginComponent,
-            // },
-            // {
-            //     path: 'all-lessons',
-            //     component: AllLessonsComponent,
-            // },
-            // {
-            //     path: 'home',
-            //     component: HomeComponent,
-            // },
-            // {
-            //     path: '',
-            //     redirectTo: 'home',
-            // },
+            {
+                path: 'course/:id',
+                component: CourseDetailComponent,
+            },
+            {
+                path: 'login',
+                component: LoginComponent,
+            },
+            {
+                path: 'all-lessons',
+                component: AllLessonsComponent,
+            },
+            {
+                path: 'home',
+                component: HomeComponent,
+            },
+            {
+                path: '',
+                redirectTo: 'home',
+            },
         ],
     },
 ];
@@ -58,7 +71,23 @@ export class Section9RoutingModule {}
 
 @NgModule({
     declarations: [Section9Component],
-    imports: [CommonModule, SharedModule, Section9RoutingModule, TopMenuModule],
+    imports: [
+        CommonModule,
+        SharedModule,
+        Section9RoutingModule,
+        HomeModule,
+        AllLessonsModule,
+        CourseDetailModule,
+        CoursesListModule,
+        LessonsListModule,
+        CourseDetailHeaderModule,
+        NewsletterModule,
+        LoginModule,
+        CourseModule,
+        TopMenuModule,
+        LessonDetailModule,
+    ],
     exports: [Section9Component],
+    providers: [DatastoreService],
 })
 export class Section9Module {}
--- ClientApp/src/app/shared/shared.module.ts
+++ ClientApp/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarItemComponent } from './sidebar/sidebar-item.component';
 import { RouterModule } from '@angular/router';
+import { SafeUrlPipe } from './pipes/safe-url.pipe';
 
 @NgModule({
-  declarations: [SidebarComponent, SidebarItemComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [SidebarComponent, SidebarItemComponent]
+    declarations: [SidebarComponent, SidebarItemComponent, SafeUrlPipe],
+    imports: [CommonModule, RouterModule],
+    exports: [SidebarComponent, SidebarItemComponent, SafeUrlPipe],
 })
 export class SharedModule {}
