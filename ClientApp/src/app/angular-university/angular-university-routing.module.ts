660e5607cf0fcaf7a2e8a0f16a88c0ce1d9ca9e2
Phil Boyd <phil.boyd@live.com>
2019-08-06 20:46:26 -0400

    NgRx in Depth - initial setup

--- ClientApp/src/app/angular-university/angular-university-routing.module.ts
+++ ClientApp/src/app/angular-university/angular-university-routing.module.ts
@@ -1,22 +1,27 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {AngularUniversityBaseComponent} from './angular-university-base.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AngularUniversityBaseComponent } from './angular-university-base.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AngularUniversityBaseComponent,
-    children: [
-      {
-        path: 'rxjs',
-        loadChildren: './rxjs-reactive-programming/au-rxjs.module#AuRxjsModule',
-      },
-    ],
-  },
+    {
+        path: '',
+        component: AngularUniversityBaseComponent,
+        children: [
+            {
+                path: 'rxjs',
+                loadChildren: './rxjs-reactive-programming/au-rxjs.module#AuRxjsModule',
+            },
+            {
+                path: 'ngrx-in-depth',
+                loadChildren: () =>
+                    import('./ngrx-in-depth/ngrx-in-depth.component').then(mod => mod.NgrxInDepthModule),
+            },
+        ],
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-  })
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
 export class AngularUniversityRoutingModule {}
---
+++ ClientApp/src/app/angular-university/ngrx-in-depth/ngrx-in-depth.component.ts
@@ -0,0 +1,53 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@Component({
+  selector: 'ngs-ngrx-in-depth',
+  template: `
+    <p>
+      ngrx-in-depth works!
+    </p>
+  `,
+})
+export class NgrxInDepthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+const routes: Routes = [
+    {
+        path: '',
+        component: NgrxInDepthComponent,
+        children: [
+            // {
+            //     path: 'ngrx-in-depth',
+            //     loadChildren: () =>
+            //         import('./ngrx-in-depth/ngrx-in-depth.component').then(mod => mod.NgrxInDepthModule),
+            // },
+        ],
+    },
+];
+
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class NgrxInDepthRoutingModule {}
+
+@NgModule({
+  declarations: [NgrxInDepthComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    NgrxInDepthRoutingModule
+  ],
+  exports: [NgrxInDepthComponent]
+})
+export class NgrxInDepthModule { }
