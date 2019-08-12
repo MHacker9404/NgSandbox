63d9f7621211f84bbe45a965f5f5fe364966868d
Phil Boyd <phil.boyd@live.com>
2019-08-12 15:38:48 -0400

    WIP

--- ClientApp/src/app/angular-university/ngrx-in-depth/section-01/courses/services/courses.service.ts
+++ ClientApp/src/app/angular-university/ngrx-in-depth/section-01/courses/services/courses.service.ts
@@ -31,15 +31,18 @@ export class CoursesService {
 
     findLessons(courseId: number, pageNumber = 0, pageSize = 3): Observable<Lesson[]> {
         return this.http
-            .get<Lesson[]>(`/api/ngrx/lessons/${courseId.toString()}/${pageNumber.toString()}/${pageSize.toString()}`, {
-                // .get<Lesson[]>('/api/ngrx/lessons', {
-                //     params: new HttpParams()
-                //         .set('courseId', courseId.toString())
-                //         .set('filter', '')
-                //         .set('sortOrder', 'asc')
-                //         .set('pageNumber', pageNumber.toString())
-                //         .set('pageSize', pageSize.toString()),
-            })
+            .get<Lesson[]>(
+                `/api/ngrx/lessons/${courseId.toString()}/${pageNumber.toString()}/${pageSize.toString()}//asc`,
+                {
+                    // .get<Lesson[]>('/api/ngrx/lessons', {
+                    //     params: new HttpParams()
+                    //         .set('courseId', courseId.toString())
+                    //         .set('filter', '')
+                    //         .set('sortOrder', 'asc')
+                    //         .set('pageNumber', pageNumber.toString())
+                    //         .set('pageSize', pageSize.toString()),
+                }
+            )
             .pipe(tag('findLessons'));
     }
 
--- ClientApp/src/app/angular-university/ngrx-in-depth/section-01/courses/services/lessons.datasource.ts
+++ ClientApp/src/app/angular-university/ngrx-in-depth/section-01/courses/services/lessons.datasource.ts
@@ -1,42 +1,32 @@
-
-
-
-import {CollectionViewer, DataSource} from "@angular/cdk/collections";
-import {Observable, BehaviorSubject, of} from "rxjs";
-import {Lesson} from "../model/lesson";
-import {CoursesService} from "./courses.service";
-import {catchError, finalize} from "rxjs/operators";
-
-
+import { CollectionViewer, DataSource } from '@angular/cdk/collections';
+import { Observable, BehaviorSubject, of } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { CoursesService } from './courses.service';
+import { catchError, finalize } from 'rxjs/operators';
 
 export class LessonsDataSource implements DataSource<Lesson> {
-
     private lessonsSubject = new BehaviorSubject<Lesson[]>([]);
 
     private loadingSubject = new BehaviorSubject<boolean>(false);
 
     public loading$ = this.loadingSubject.asObservable();
 
-    constructor(private coursesService: CoursesService) {
+    constructor(private coursesService: CoursesService) {}
 
-    }
-
-    loadLessons(courseId:number,
-                pageIndex:number,
-                pageSize:number) {
-
+    loadLessons(courseId: number, pageIndex: number, pageSize: number) {
         this.loadingSubject.next(true);
 
-        this.coursesService.findLessons(courseId, pageIndex, pageSize).pipe(
+        this.coursesService
+            .findLessons(courseId, pageIndex, pageSize)
+            .pipe(
                 catchError(() => of([])),
                 finalize(() => this.loadingSubject.next(false))
             )
             .subscribe(lessons => this.lessonsSubject.next(lessons));
-
     }
 
     connect(collectionViewer: CollectionViewer): Observable<Lesson[]> {
-        console.log("Connecting data source");
+        console.log('Connecting data source');
         return this.lessonsSubject.asObservable();
     }
 
@@ -44,6 +34,4 @@ export class LessonsDataSource implements DataSource<Lesson> {
         this.lessonsSubject.complete();
         this.loadingSubject.complete();
     }
-
 }
-
