ea4e06ed21b81d25f4c668f8139d2897b639fa1c
Phil Boyd <phil.boyd@live.com>
2019-08-28 15:14:26 -0400

    WIP - Lesson 26 complete - coursesState defined

--- ClientApp/src/app/angular-university/ngrx-in-depth/section-05/courses/state/reducer.ts
+++ ClientApp/src/app/angular-university/ngrx-in-depth/section-05/courses/state/reducer.ts
@@ -2,15 +2,22 @@ import { Action } from '@ngrx/store';
 import * as fromApp from 'src/app/state/index';
 import * as fromParent from '../../state/reducer';
 import { CourseActions } from './actions';
+import { ICourse } from '../../model/course';
 
 export const coursesFeatureKey = `${fromParent.section05FeatureKey}:courses`;
 
 export interface CoursesState extends fromApp.AppState {
-    courses: {};
+    courses: {
+        entities: { [key: number]: ICourse };
+        order: number[];
+    };
 }
 
 export const initialState: CoursesState = {
-    courses: {},
+    courses: {
+        entities: {},
+        order: [],
+    },
 };
 
 export function reducer(state: CoursesState = initialState, action: CourseActions): CoursesState {
