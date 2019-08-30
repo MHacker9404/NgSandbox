6fb074bc5985c8f25a40291acfe0961fc791cb88
Phil Boyd <phil.boyd@live.com>
2019-08-30 07:31:31 -0400

    WIP - WebApis updated, reducer implemented

--- ClientApp/src/app/angular-university/ngrx-in-depth/section-05/courses/state/reducer.ts
+++ ClientApp/src/app/angular-university/ngrx-in-depth/section-05/courses/state/reducer.ts
@@ -8,23 +8,11 @@ export const coursesFeatureKey = `${fromParent.section05FeatureKey}:courses`;
 
 export interface CoursesState extends EntityState<ICourse> {}
 
-export const adapter: EntityAdapter<ICourse> = createEntityAdapter<ICourse>();
+const adapter: EntityAdapter<ICourse> = createEntityAdapter<ICourse>();
 
-// export interface CoursesState extends fromApp.AppState {
-// courses: {
-//     entities: { [key: number]: ICourse };
-//     order: number[];
-// };
-// }
+const initialState: CoursesState = adapter.getInitialState();
 
-// export const initialState: CoursesState = {
-//     courses: {
-//         entities: {},
-//         order: [],
-//     },
-// };
-
-export function reducer(state: CoursesState, action: CourseActions): CoursesState {
+export function reducer(state: CoursesState = initialState, action: CourseActions): CoursesState {
     switch (action.type) {
         case CoursesActionTypes.LoadCourse: {
             const course = _deepClone(action.payload.course);
@@ -40,3 +28,17 @@ export function reducer(state: CoursesState, action: CourseActions): CoursesStat
         }
     }
 }
+
+// export interface CoursesState extends fromApp.AppState {
+// courses: {
+//     entities: { [key: number]: ICourse };
+//     order: number[];
+// };
+// }
+
+// export const initialState: CoursesState = {
+//     courses: {
+//         entities: {},
+//         order: [],
+//     },
+// };
