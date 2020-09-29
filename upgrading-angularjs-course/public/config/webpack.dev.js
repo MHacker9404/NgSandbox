38c5c8b59e8901ebc798988faa6196e8fa0db94b
Phil Boyd <phil.boyd@prb-services-llc.com>
2020-09-29 11:23:41 -0400

    lesson 2-55 complete

--- upgrading-angularjs-course/public/config/webpack.dev.js
+++ upgrading-angularjs-course/public/config/webpack.dev.js
@@ -15,9 +15,53 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                        },
+                    },
+                    {
+                        loader: '@peterek/extract-loader',
+                    },
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(jpg|gif|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
         ],
     },
     resolve: {
         extensions: ['.ts', '.js', '.json'],
+    },
+    devServer: {
+        contentBase: './',
+        overlay: true,
+        port: 9000,
     },
 };
