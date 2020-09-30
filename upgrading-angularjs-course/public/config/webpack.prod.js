7ea94c856a8674b9d0dfc4222014f8cc714e6421
Phil Boyd <phil.boyd@prb-services-llc.com>
2020-09-30 08:25:55 -0400

    lesson 2-88 complete

--- upgrading-angularjs-course/public/config/webpack.prod.js
+++ upgrading-angularjs-course/public/config/webpack.prod.js
@@ -1,3 +1,14 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
     mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+    },
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+        splitChunks: {
+            name: true,
+        },
+    },
 };
