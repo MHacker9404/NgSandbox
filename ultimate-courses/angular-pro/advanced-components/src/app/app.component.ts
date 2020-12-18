63e87b696d3888d5814b69951e284addab8c6ba9
Phil Boyd <phil.boyd@prb-services-llc.com>
2020-12-18 17:31:51 -0500

    dynamic components

--- ultimate-courses/angular-pro/advanced-components/src/app/app.component.ts
+++ ultimate-courses/angular-pro/advanced-components/src/app/app.component.ts
@@ -1,23 +1,57 @@
-import { Component } from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    ComponentFactoryResolver,
+    ComponentRef,
+    ViewChild,
+    ViewContainerRef,
+} from '@angular/core';
 import { User } from './auth-form/auth-form.interface';
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     title = 'content-projection';
+    @ViewChild('entry', { read: ViewContainerRef }) entry!: ViewContainerRef;
+    component!: ComponentRef<AuthFormComponent>;
+    // @ViewChild('entry') entry!: ViewContainerRef;
 
-    createUser(user: User) {
-        alert(`Create account: ${JSON.stringify(user)}`);
-    }
+    constructor(private _resolver: ComponentFactoryResolver, private _cd: ChangeDetectorRef) {}
 
     loginUser(user: User) {
         alert(`Login: ${JSON.stringify(user)}`);
     }
 
-    onChecked(value: boolean) {
-        alert(`checked: ${value}`);
+    ngAfterViewInit() {
+        const factory = this._resolver.resolveComponentFactory(AuthFormComponent);
+        this.entry.createComponent<AuthFormComponent>(factory);
+        this.component = this.entry.createComponent<AuthFormComponent>(factory, 0);
+        console.debug(this.component.instance);
+
+        this.component.instance.title = 'changed';
+        this.component.instance.submitted.subscribe(this.loginUser);
+
+        this._cd.detectChanges();
     }
+
+    destroyComponent() {
+        this.component.destroy();
+    }
+
+    moveComponent() {
+        this.entry.move(this.component.hostView, 1);
+    }
+
+    // createUser(user: User) {
+    //     alert(`Create account: ${JSON.stringify(user)}`);
+    // }
+
+    // onChecked(value: boolean) {
+    //     alert(`checked: ${value}`);
+    // }
 }
--- ultimate-courses/angular-pro/advanced-components/src/app/auth-form/auth-form.component.ts
+++ ultimate-courses/angular-pro/advanced-components/src/app/auth-form/auth-form.component.ts
@@ -29,7 +29,8 @@ import { User } from './auth-form.interface';
     template: `
         <div>
             <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
-                <ng-content select="h3"></ng-content>
+                <h3>{{ title }}</h3>
+                <!-- <ng-content select="h3"></ng-content> -->
                 <label>
                     Email address
                     <input #email type="email" name="email" ngModel />
@@ -38,25 +39,27 @@ import { User } from './auth-form.interface';
                     Password
                     <input type="password" name="password" ngModel />
                 </label>
-                <ng-content select="app-auth-remember"></ng-content>
+                <!-- <ng-content select="app-auth-remember"></ng-content>
                 <prb-auth-message [style.display]="showMessage ? 'inherit' : 'none'"></prb-auth-message>
                 <prb-auth-message [style.display]="showMessage ? 'inherit' : 'none'"></prb-auth-message>
-                <ng-content select="button"></ng-content>
+                <ng-content select="button"></ng-content> -->
+                <button type="submit">Login</button>
             </form>
         </div>
     `,
 })
 export class AuthFormComponent implements AfterContentInit, AfterViewInit {
     @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
-    //  for content projected into component
-    @ContentChildren(AuthRememberComponent)
-    remember: QueryList<AuthRememberComponent> | undefined;
-    @ViewChildren(AuthMessageComponent) message: QueryList<AuthMessageComponent> | undefined;
-    @ViewChild('email') email: ElementRef | undefined;
+    // //  for content projected into component
+    // @ContentChildren(AuthRememberComponent)
+    // remember: QueryList<AuthRememberComponent> | undefined;
+    // @ViewChildren(AuthMessageComponent) message: QueryList<AuthMessageComponent> | undefined;
+    // @ViewChild('email') email: ElementRef | undefined;
 
     constructor(private _cd: ChangeDetectorRef, private _renderer: Renderer2) {}
 
     showMessage: boolean = false;
+    title = 'Login';
 
     onSubmit(value: User) {
         this.submitted.emit(value);
@@ -64,12 +67,12 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
 
     //  for content projected into component
     ngAfterContentInit(): void {
-        if (this.remember) {
-            // this.remember.checked.subscribe((checked: boolean) => (this.showMessage = checked));
-            this.remember.forEach((item: AuthRememberComponent) =>
-                item.checked.subscribe((checked: boolean) => (this.showMessage = checked))
-            );
-        }
+        // if (this.remember) {
+        //     // this.remember.checked.subscribe((checked: boolean) => (this.showMessage = checked));
+        //     this.remember.forEach((item: AuthRememberComponent) =>
+        //         item.checked.subscribe((checked: boolean) => (this.showMessage = checked))
+        //     );
+        // }
     }
 
     ngAfterViewInit(): void {
@@ -79,13 +82,12 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
         // this.email?.nativeElement.focus();
         // this._renderer.setAttribute(this.email, 'placeholder', 'Enter your email address');
         // this._renderer.addClass(this.email, 'email');
-
-        if (this.message) {
-            // setTimeout(() => {
-            //     this.message!.forEach((message: AuthMessageComponent) => (message.days = 30));
-            // });
-            this.message!.forEach((message: AuthMessageComponent) => (message.days = 30));
-            this._cd.detectChanges();
-        }
+        // if (this.message) {
+        //     // setTimeout(() => {
+        //     //     this.message!.forEach((message: AuthMessageComponent) => (message.days = 30));
+        //     // });
+        //     this.message!.forEach((message: AuthMessageComponent) => (message.days = 30));
+        //     this._cd.detectChanges();
+        // }
     }
 }
