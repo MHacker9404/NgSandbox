08e9dfd23ae27532f8735768c96cb84956a880db
Phil Boyd <phil.boyd@prb-services-llc.com>
2020-12-18 13:33:29 -0500

    05 complete - changing strategy a little

---
+++ ultimate-courses/angular-pro/05-content-children-querylists - Copy/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AuthFormModule } from './auth-form/auth-form.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, AuthFormModule],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
